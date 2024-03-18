f3cc4bee759493f67029ff2f4bb17299b771f01c
Stian Haugland <stiahau@gmail.com>
2024-03-18 22:48:30 +0100

    fix premiumcalculator and and tests

--- Claims/DomainLogic/PremiumCalculator.cs
+++ Claims/DomainLogic/PremiumCalculator.cs
@@ -1,38 +1,47 @@
 using Claims;
 
-public static class PremiumCalculator {
-    
-public static decimal ComputePremium(DateOnly startDate, DateOnly endDate, CoverType coverType)
+public static class PremiumCalculator
+{
+
+    public static decimal ComputePremium(DateOnly startDate, DateOnly endDate, CoverType coverType)
     {
-        var multiplier = 1.3m;
-        if (coverType == CoverType.Yacht)
+        int baseDayRate = 1250;
+
+        var coverTypeToMultiplier = new Dictionary<CoverType, decimal>{
+            { CoverType.Yacht, 1.1m },
+            { CoverType.PassengerShip, 1.2m },
+            { CoverType.Tanker, 1.5m }
+        };
+        decimal defaultMultiplier = 1.3m;
+
+        decimal multiplier = coverTypeToMultiplier.ContainsKey(coverType) ? coverTypeToMultiplier[coverType] : defaultMultiplier;
+
+        decimal premiumPerDay = baseDayRate * multiplier;
+        
+        // Expects that when the startDate and end date is the same it is a single day
+        decimal insuranceLength = endDate.DayNumber - startDate.DayNumber + 1;
+
+        decimal discountNext150Days = coverType == CoverType.Yacht ? 0.05m : 0.02m;
+        decimal discountNext150DaysFactor = 1 - discountNext150Days;
+        
+        decimal discountRemainingDays = coverType == CoverType.Yacht ? 0.08m : 0.03m;
+        decimal discountRemainingDaysFactor = 1 - discountRemainingDays;
+
+        decimal priceFirst30Days = Math.Min(insuranceLength, 30) * premiumPerDay;
+        if (insuranceLength <= 30)
         {
-            multiplier = 1.1m;
+            return priceFirst30Days;
         }
 
-        if (coverType == CoverType.PassengerShip)
+        decimal remainingDays = insuranceLength - 30;
+        decimal priceNext150Days = Math.Min(remainingDays, 150) * premiumPerDay * discountNext150DaysFactor;
+        if (insuranceLength <= 180)
         {
-            multiplier = 1.2m;
+            return priceFirst30Days + priceNext150Days;
         }
 
-        if (coverType == CoverType.Tanker)
-        {
-            multiplier = 1.5m;
-        }
-
-        var premiumPerDay = 1250 * multiplier;
-        var insuranceLength = endDate.DayNumber - startDate.DayNumber;
-        var totalPremium = 0m;
-
-        for (var i = 0; i < insuranceLength; i++)
-        {
-            if (i < 30) totalPremium += premiumPerDay;
-            if (i < 180 && coverType == CoverType.Yacht) totalPremium += premiumPerDay - premiumPerDay * 0.05m;
-            else if (i < 180) totalPremium += premiumPerDay - premiumPerDay * 0.02m;
-            if (i < 365 && coverType != CoverType.Yacht) totalPremium += premiumPerDay - premiumPerDay * 0.03m;
-            else if (i < 365) totalPremium += premiumPerDay - premiumPerDay * 0.08m;
-        }
-
-        return totalPremium;
+        remainingDays -= 150;
+        decimal priceRemainingDays = remainingDays * premiumPerDay * discountRemainingDaysFactor;
+        return priceFirst30Days + priceNext150Days + priceRemainingDays;
     }
 }
